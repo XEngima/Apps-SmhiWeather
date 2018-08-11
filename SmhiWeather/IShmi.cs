8930df24f557c41d7cc82e1a506adb67e20dba94
SCBDEIS <SCBDEIS@CLD10302>
2018-08-11 07:05:37 +0200

    Added interface ISmhi for testing purposes.

---
+++ SmhiWeather/IShmi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmhiWeather
+{
+    public interface IShmi
+    {
+        decimal CoordLat { get; }
+
+        decimal CoordLon { get; }
+
+        Forecast GetForecast();
+
+        ForecastTimeSerie GetCurrentWeather();
+    }
+}
